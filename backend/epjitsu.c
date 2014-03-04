5d568352683cf16fac98317f75cf2b08b4baf584
Peter Marschall <peter@adpm.de>
2014-03-04 14:09:06 +0100

    epjitsu: use ternary ?: operator in some functions

--- backend/epjitsu.c
+++ backend/epjitsu.c
@@ -4863,11 +4863,7 @@ static int
 get_page_width(struct scanner *s) 
 {
   /* scanner max for fb */
-  if(s->source == SOURCE_FLATBED){
-      return s->max_x;
-  }
-
-  return s->page_width;
+  return (s->source == SOURCE_FLATBED) ? s->max_x : s->page_width;
 }
 
 /* s->page_height stores the user setting
@@ -4879,10 +4875,6 @@ static int
 get_page_height(struct scanner *s) 
 {
   /* scanner max for fb */
-  if(s->source == SOURCE_FLATBED){
-      return s->max_y;
-  }
-
-  return s->page_height;
+  return (s->source == SOURCE_FLATBED) ? s->max_y : s->page_height;
 }
 
